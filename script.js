7311f3b65f5c30f6f0912aa4512183efc9a9ff51
Ryan Foster-Hill <rfoster-hill@hotmail.com>
2023-07-14 11:54:30 +0100

    added button anims, player name colours

--- script.js
+++ script.js
@@ -43,6 +43,14 @@ const gameController = {
     playerCanRoll: [true],
     numOfExtPlayers: 0,
 
+    //Applies button animation
+    buttonAnimation(buttonElement) {     
+        buttonElement.style.border = "5px inset";
+        setTimeout(() => {
+            buttonElement.style.border = "5px outset";
+        }, 250)
+    },
+
     //Handles the start menu
     startMenuController() {
         //Makes start menu hidden and game container visible
@@ -55,10 +63,18 @@ const gameController = {
 
     //Handles creation of new game containers
     createDuplicateGame(playerScoresArray) {
-        let ranColours = ["red", "blue", "green", "purple", "orange", "white", "yellow"]
+
+        //Applies button animation
+        this.buttonAnimation(document.getElementById("createNewGameButton"));
+
+        //Array of background colours
+        let ranColours = ["blue", "green", "purple", "orange", "white", "yellow"]
 
         //Create a duplicate of game container
         let duplicateGame = document.getElementsByClassName("gameContainer")[0].cloneNode(true);
+
+        //Changes the player name color to default black, it would be red normally as its copied
+        duplicateGame.getElementsByClassName("playerName")[0].style.color = "black";
 
         //Gives the new game container a random background colour
         duplicateGame.style.backgroundColor = ranColours[Math.floor(Math.random() * 7)];
@@ -79,15 +95,19 @@ const gameController = {
         document.getElementsByClassName("playerName")[playerScoresArray.length - 1].textContent = `Player: ${playerScoresArray.length}`;
     },
 
+
     //Handles the gameplay
     runGame(playerScore, playerNumber) {
         let diceRoll = 0;
-
+        
         //Creates click event listener
         document.getElementsByClassName("diceRollButton")[playerNumber].addEventListener("click", (event) => {
 
             //Checks if player can role and if its their turn to roll
             if (this.playerCanRoll[playerNumber] === true && this.playerTurn === playerNumber) {
+                
+                //Applies button animation
+                this.buttonAnimation(document.getElementsByClassName("diceRollButton")[playerNumber]);
 
                 //Hides result text for all players, shows dice image, calls for a dice roll, adds dice roll to player score, 
                 //updates current score text
@@ -99,20 +119,47 @@ const gameController = {
                 playerScore[playerNumber] += diceRoll;
                 document.getElementsByClassName("currentScoreText")[playerNumber].textContent = `Current Score : ${playerScore[playerNumber]}`;
 
-                //A score of 20 means the player wins
-                if (playerScore[playerNumber] > 20) {
-                    document.getElementsByClassName("resultText")[playerNumber].textContent = "You Win";
-                    document.getElementsByClassName("resultText")[playerNumber].style.visibility = "visible";
-                    playerScore[playerNumber] = 0;
-                    this.playerTurn = 0;
-                    for (let i = 0; i < this.numOfExtPlayers + 1; i ++) {
-                        this.playerCanRoll[i] = true;
-                        playerScore[i] = 0;
-                        document.getElementsByClassName("currentScoreText")[i].textContent = `Current Score : 0`;
-                        document.getElementsByClassName("totalScoreText")[i].textContent = `Total Score : 0`;
+                //Controls game wins, checks if game is in single player or mutliplayer mode by checking amount of players
+                if (amountOfExtraPlayers === 0) {
+                    //In single player, a score of greater than 20 wins
+                    if (playerScore[playerNumber] > 20) {
+                        document.getElementsByClassName("resultText")[playerNumber].textContent = "You Win";
+                        document.getElementsByClassName("resultText")[playerNumber].style.visibility = "visible";
+                        playerScore[playerNumber] = 0;
+                        this.playerTurn = 0;
+                        
+                        //Resets every player to default settings
+                        for (let i = 0; i < this.numOfExtPlayers + 1; i ++) {
+                            this.playerCanRoll[i] = true;
+                            playerScore[i] = 0;
+                            document.getElementsByClassName("currentScoreText")[i].textContent = `Current Score : 0`;
+                            document.getElementsByClassName("totalScoreText")[i].textContent = `Total Score : 0`;
+                            document.getElementsByClassName("playerName")[playerNumber].style.color = "black";
+                        }
+                        document.getElementsByClassName("playerName")[0].style.color = "red";
+                        return;
                     }
-                    return;
+                } else {
+                    //In multiplayer, a score of 20 or more wins
+                    if (playerScore[playerNumber] >= 20) {
+                        document.getElementsByClassName("resultText")[playerNumber].textContent = "You Win";
+                        document.getElementsByClassName("resultText")[playerNumber].style.visibility = "visible";
+                        playerScore[playerNumber] = 0;
+                        this.playerTurn = 0;
+                        
+                        //Resets every player to default settings
+                        for (let i = 0; i < this.numOfExtPlayers + 1; i ++) {
+                            this.playerCanRoll[i] = true;
+                            playerScore[i] = 0;
+                            document.getElementsByClassName("currentScoreText")[i].textContent = `Current Score : 0`;
+                            document.getElementsByClassName("totalScoreText")[i].textContent = `Total Score : 0`;
+                            document.getElementsByClassName("playerName")[playerNumber].style.color = "black";
+                        }
+                        document.getElementsByClassName("playerName")[0].style.color = "red";
+                        return;
+                    }
                 }
+
 
                 //A roll of one means the game is over and resets
                 if (diceRoll === 1) {
@@ -123,12 +170,15 @@ const gameController = {
                     document.getElementsByClassName("resultText")[playerNumber].style.visibility = "visible";
                     playerScore[playerNumber] = 0;
                     this.playerTurn = 0;
+                    //Resets every player to default settings
                     for (let i = 0; i < this.numOfExtPlayers + 1; i ++) {
                         this.playerCanRoll[i] = true;
                         playerScore[i] = 0;
                         document.getElementsByClassName("currentScoreText")[i].textContent = `Current Score : 0`;
                         document.getElementsByClassName("totalScoreText")[i].textContent = `Total Score : 0`;
+                        document.getElementsByClassName("playerName")[playerNumber].style.color = "black";
                     }
+                    document.getElementsByClassName("playerName")[0].style.color = "red";
                     return;
                 }
             }
@@ -141,7 +191,19 @@ const gameController = {
             //and makes sure there are more than 1 players in the game by checking the amount of holdScoreButtons in existence is 
             //greater than 1 (because hold button is useless in single player game)
             if (this.playerCanRoll[playerNumber] === true && this.playerTurn === playerNumber && this.numOfExtPlayers > 0) {
+                
+                //Applies button animation
+                this.buttonAnimation(document.getElementsByClassName("holdScoreButton")[playerNumber]);
+
+                //Changes colour of current players name to black and the next players name to red, and updates total score
+                document.getElementsByClassName("playerName")[playerNumber].style.color = "black";
+                if (playerNumber !== amountOfExtraPlayers) {
+                    document.getElementsByClassName("playerName")[playerNumber + 1].style.color = "red";
+                } else {
+                    document.getElementsByClassName("playerName")[0].style.color = "red";
+                }
                 document.getElementsByClassName("totalScoreText")[playerNumber].textContent = `Total Score : ${playerScore[playerNumber]}`;
+                
                 this.playerCanRoll[playerNumber] = false;
                 if (this.playerTurn === this.numOfExtPlayers) {
                     this.playerTurn = 0;
@@ -149,7 +211,6 @@ const gameController = {
                     //Resets all players to be able to roll
                     for (let i = 0; i < this.playerCanRoll.length; i ++) {
                         this.playerCanRoll[i] = true;
-                        playerScore[i] = 0;
                     }
                 } else {
                     this.playerTurn++;
